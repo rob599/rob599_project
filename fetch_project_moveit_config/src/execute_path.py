91189ed756ec5e8b3cea48b34fb5abc412c9da45
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-10 16:24:10 -0800

    changed file name

--- fetch_project_moveit_config/src/execute_path.py
+++ fetch_project_moveit_config/src/execute_path.py
@@ -13,53 +13,52 @@ from moveit_python import MoveGroupInterface, PlanningSceneInterface
 from std_msgs.msg import String, Int16
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, PoseArray
 
-class MoveGroupInteface(object):
-  """MoveGroupPythonInteface"""
+class ExecutePath(object):
+
   def __init__(self):
-    super(MoveGroupInteface, self).__init__()
-    self.gui_input_sub = rospy.Subscriber('gui_input', String, self.callback)
-    self.waypoints_sub = rospy.Subscriber('waypoints', PoseArray, self.plan_cartesian_path)
-    ## First initialize `moveit_commander`
+    super(ExecutePath, self).__init__()
+    self.gui_input_sub = rospy.Subscriber('gui_input', String, self.interface_callback)
+    self.waypoints_sub = rospy.Subscriber('waypoints', PoseArray, self.waypoint_callback)
+
+    # First initialize `moveit_commander`
     moveit_commander.roscpp_initialize(sys.argv)
-    ## Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
+
+    # Instantiate a `RobotCommander`_ object. This object is the outer-level
+    # interface to the robot
     self.robot = moveit_commander.RobotCommander()
-    ## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
-    ## to the world surrounding the robot:
-    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-    ## to one group of joints.  In this case the group is the joints in the Fetch
-    ## arm so we set ``group_name = panda_arm``. If you are using a different robot,
-    ## you should change this value to the name of your robot arm planning group.
-    ## This interface can be used to plan and execute motions on the Panda:
+
+    # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+    # to one group of joints.
     self.group = moveit_commander.MoveGroupCommander("arm_with_torso")
     self.group.set_end_effector_link("gripper_link")
-    ## We create a `DisplayTrajectory`_ publisher which is used later to publish
-    ## trajectories for RViz to visualize:
+
+    # We create a `DisplayTrajectory`_ publisher which is used later to publish
+    # trajectories for RViz to visualize:
     self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                    moveit_msgs.msg.DisplayTrajectory,
                                                    queue_size=20)
-    ## Getting Basic Information
-    self.planning_frame = self.group.get_planning_frame()
-    # We can also print the name of the end-effector link for this group:
 
-    # We can get a list of all the groups in the robot:
-    # group_names = robot.get_group_names()
-    # Misc variables
-    # self.box_name = ''
-    # self.robot = robot
-    # self.scene = scene
-    # self.group = group
-    # self.display_trajectory_publisher = display_trajectory_publisher
-    # self.planning_frame = planning_frame
-    # self.eef_link = "gripper_link"
-    # print(eef_link)
-    # self.group_names = group_names
-    # self.plan = None
+    self.waypoints = None
+    self.plan = None
+
+    # Getting Basic Information
+    self.planning_frame = self.group.get_planning_frame()
+
     self.path_to_goal=FollowTrajectoryClient()
 
 
-  def callback(self,gui_input):
+  def waypoint_callback(self,msg):
+      self.waypoints = []
+      for i in range(len(msg.poses)):
+          self.waypoints.append(msg.poses[i])
 
-      if gui_input.data == "1":
+
+  def interface_callback(self,gui_input):
+
+      if gui_input.data == "0":
+          self.plan_cartesian_path()
+
+      elif gui_input.data == "1":
           self.execute_plan()
 
       elif gui_input.data == "2":
@@ -68,19 +67,12 @@ class MoveGroupInteface(object):
       elif gui_input.data == "3":
           self.path_to_goal.tuck_pose()
 
-  def plan_cartesian_path(self,waypoints):
+  def plan_cartesian_path(self):
     ## Cartesian Paths
-
-    # waypoints = []
-    # for i in range(len(msg.poses)):
-    #     waypoints.append(msg.poses[i])
-    # We want the Cartesian path to be interpolated at a resolution of 1 cm
-    # which is why we will specify 0.01 as the eef_step in Cartesian
-    # translation.  We will disable the jump threshold by setting it to 0.0 disabling:
     (plan, fraction) = self.group.compute_cartesian_path(
-                                       waypoints,   # waypoints to follow
-                                       0.1,         # eef_step
-                                       0.00)        # jump_threshold
+                                       self.waypoints,   # waypoints to follow
+                                       0.1,              # eef_step
+                                       0.00)             # jump_threshold
 
     self.plan = self.group.retime_trajectory(self.robot.get_current_state(),plan,.2)
     # Note: We are just planning, not asking move_group to actually move the robot yet:
@@ -142,6 +134,6 @@ class FollowTrajectoryClient(object):
 
 
 if __name__=="__main__":
-    rospy.init_node('movegroupinterface',anonymous=True)
-    MoveGroupInteface()
+    rospy.init_node('execute_path',anonymous=True)
+    ExecutePath()
     rospy.spin()
