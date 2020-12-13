79dc270eb0790e201c58a91035a9a20fbecf7fa7
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-12 16:42:09 -0800

    Include comments throughtout script

--- fetch_project_moveit_config/src/waypoint_generator.py
+++ fetch_project_moveit_config/src/waypoint_generator.py
@@ -2,7 +2,6 @@
 
 # Import what we need
 import rospy
-import math
 import random
 import numpy as np
 
@@ -21,24 +20,28 @@ class Waypoint_generator:
         # Initialize Publishers
         self.waypoints_pub        = rospy.Publisher('waypoints'       , PoseArray        , queue_size=1)
         self.waypoints_marker_pub = rospy.Publisher('waypoints_marker', Marker           , queue_size=1)
-        self.waypoints_missed     = rospy.Publisher('waypoints_missed', numpy_msg(Floats), queue_size=1)
+        self.missed_data          = rospy.Publisher('missed_data', numpy_msg(Floats), queue_size=1)
 
         # Setup header
         self.header = Header()
         self.header.frame_id = "/base_link"
         self.header.stamp = rospy.Time.now()
 
+        # Intialize the inverse matrix
         self.M_inv = None
 
+        # Intialize the X and Y coordinates of the 2D data points
         self.X = None
         self.Y = None
 
-        # self.coodinates_2D = None
+        # Set the waypoint resolution (distance between points)
         self.resolution = .05
 
+        # Initialize waypoints as a PoseArray
         self.waypoints = PoseArray()
         self.waypoints.header = self.header
 
+        # Initialize waypoint_markers and all of the other feature values
         self.waypoints_marker = Marker()
         self.waypoints_marker.header = self.header
         self.waypoints_marker.type = Marker.LINE_STRIP
@@ -54,32 +57,39 @@ class Waypoint_generator:
 
 
     def data_callback(self, data_msg):
+        # Splice the data: Inverse matrix and the 2D coordinates
         M_inv_arr = data_msg.data[:16]
         coord_2D_arr = data_msg.data[16:]
         coord_arr_len = len(coord_2D_arr)
 
         # Set Inverse transfer Matrix (M_inv) to equal an empty matrix.
         self.M_inv = np.empty(shape=[4,4])
-        # self.coordinates_2D = np.empty(shape=[coord_arr_len,2])
+
+        # assign X and Y to empty lists
         self.X = []
         self.Y = []
 
+        # Fill in the the inverse matrix with the subscribed data
         for i in range(4):
             self.M_inv[i][0] = M_inv_arr[i*4 + 0]
             self.M_inv[i][1] = M_inv_arr[i*4 + 1]
             self.M_inv[i][2] = M_inv_arr[i*4 + 2]
             self.M_inv[i][3] = M_inv_arr[i*4 + 3]
 
+        # Append the X and Y values of the coordinates positions
         for i in range(coord_arr_len):
             if (i % 2) == 0:
                 self.X.append(coord_2D_arr[i])
             else:
                 self.Y.append(coord_2D_arr[i])
 
+            # The planner needs a closed loop of points, hence why the first set
+            # of coordinates are appended at the end of the lists.
             if (i+1) == coord_arr_len:
                 self.X.append(coord_2D_arr[0])
                 self.Y.append(coord_2D_arr[1])
-        # print(self.X, self.Y)
+
+        # Run waypoint_planner function
         self.waypoint_planner()
 
 
@@ -113,6 +123,7 @@ class Waypoint_generator:
             # Append position values for the marker
             marker_list.append(Point(p.position.x, p.position.y, p.position.z))
 
+        # assisn poses to the PoseArray, self,waypoints.
         self.waypoints.poses = poses
 
         # Publish poses for computeCartesianPath
@@ -134,7 +145,7 @@ class Waypoint_generator:
         # Publish the inverse matrix and missed points for optimization node
         a = np.array(self.M_inv.ravel(), dtype=np.float32)
         b = np.array(missed_points.ravel(), dtype=np.float32)
-        self.waypoints_missed.publish(np.array(pose_arr.ravel(), dtype=np.float32))
+        self.missed_data.publish(np.concatenate((a,b)))
 
 
 if __name__=="__main__":
