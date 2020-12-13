8e1bf49a51e195256eb402035c0156e6ae878b97
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-12 16:53:34 -0800

    Include comments on script

--- fetch_project_moveit_config/src/optimizer.py
+++ fetch_project_moveit_config/src/optimizer.py
@@ -7,7 +7,6 @@ sys.modules['sklearn.externals.six'] = six
 import mlrose
 import numpy as np
 import rospy
-import math
 import random
 
 from visualization_msgs.msg import Marker
@@ -15,81 +14,91 @@ from geometry_msgs.msg import Pose,PoseArray,Quaternion,Point
 from std_msgs.msg import Header
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
-from grid_based_sweep_coverage_path_planner import planning_animation,planning
 
-class Waypoint_generator:
+class Optimizer:
     def __init__(self):
         # Initialize Subscribers
-        self.waypoints_missed_sub = rospy.Subscriber('waypoints_missed',numpy_msg(Floats), self.data_callback, queue_size=1)
+        self.waypoints_missed_sub = rospy.Subscriber('missed_data',numpy_msg(Floats), self.data_callback, queue_size=1)
 
         # Initialize Publishers
-        self.waypoints_pub        = rospy.Publisher('waypoints'       , PoseArray, queue_size=1)
-        self.waypoints_marker_pub = rospy.Publisher('waypoints_marker', Marker   , queue_size=1)
+        self.waypoints_pub = rospy.Publisher('waypoints_missed', PoseArray, queue_size=1)
 
         # Setup header
         self.header = Header()
         self.header.frame_id = "/base_link"
         self.header.stamp = rospy.Time.now()
 
+        # Intialize inverse Matrix to None
         self.M_inv = None
 
-        self.X = None
-        self.Y = None
+        # Intialize the coordinates list of missed points
+        self.coords_list = []
 
-        # self.coodinates_2D = None
-        self.resolution = .05
-
-        self.waypoints = PoseArray()
-        self.waypoints.header = self.header
+        # Intialize waypoints_missed as a PoseArray
+        self.waypoints_missed = PoseArray()
+        self.waypoints_missed.header = self.header
 
 
-def data_callback(self, data_msg):
-    M_inv_arr = data_msg.data[:16]
-    missed_arr = data_msg.data[16:]
-    len_missed = int(len(missed_arr)/2)
-    print(len_missed)
-    # Set Inverse transfer Matrix (M_inv) to equal an empty matrix.
-    self.M_inv = np.empty(shape=[4,4])
+    def data_callback(self, data_msg):
+        # Splice the data: Inverse matrix and the 2D coordinates of the missed points
+        M_inv_arr  = data_msg.data[:16]
+        missed_arr = data_msg.data[16:]
+        len_missed = int(len(missed_arr)/2)
 
-    rand_list = []
+        self.coords_list = []
 
-    for i in range(len_missed):
-        rand_list.append((missed_arr[i*2 + 0], missed_arr[i*2 + 1]))
+        # Set Inverse transfer Matrix (M_inv) to equal an empty matrix.
+        self.M_inv = np.empty(shape=[4,4])
 
-    for i in range(4):
-        self.M_inv[i][0] = M_inv_arr[i*4 + 0]
-        self.M_inv[i][1] = M_inv_arr[i*4 + 1]
-        self.M_inv[i][2] = M_inv_arr[i*4 + 2]
-        self.M_inv[i][3] = M_inv_arr[i*4 + 3]
+        # Fill in the the inverse matrix with the subscribed data
+        for i in range(4):
+            self.M_inv[i][0] = M_inv_arr[i*4 + 0]
+            self.M_inv[i][1] = M_inv_arr[i*4 + 1]
+            self.M_inv[i][2] = M_inv_arr[i*4 + 2]
+            self.M_inv[i][3] = M_inv_arr[i*4 + 3]
 
-#     for e in rand_list:
-#         if (i % 2) == 0:
-#             self.X.append(coord_2D_arr[i])
-#         else:
-#             self.Y.append(coord_2D_arr[i])
-#
-#         if (i+1) == coord_arr_len:
-#             self.X.append(coord_2D_arr[0])
-#             self.Y.append(coord_2D_arr[1])
-#     # print(self.X, self.Y)
-#     self.waypoint_planner()
-#
-#
-#
-# def waypoint_planner(self):
-#     #Create list of city coordinates
-#     coords_list = [(1, 1), (4, 2), (5, 2), (6, 4), (4, 4), (3, 6), (1, 5), (2, 3)]
-#
-#     # Initialize fitness function object using coords_list
-#     fitness_coords = mlrose.TravellingSales(coords = coords_list)
-#
-#     problem_fit = mlrose.TSPOpt(length = 8,
-#                                 fitness_fn = fitness_coords,
-#                                 maximize=False)
-#
-#     # Solve problem using the genetic algorithm
-#     best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state = 2)
-#
-#     print('The best state found is: ', best_state)
-#
-#     print('The fitness at the best state is: ', best_fitness)
+        # Store the x and y positions of the missed points to self.coords_list
+        for i in range(len_missed):
+            self.coords_list.append((missed_arr[i*2 + 0], missed_arr[i*2 + 1]))
+
+        # Run optimize_path function
+        self.optimize_path()
+
+    def optimize_path(self):
+        # Initialize fitness function object using self.coords_list
+        fitness_coords = mlrose.TravellingSales(coords = self.coords_list)
+
+        problem_fit = mlrose.TSPOpt(length = 3,
+                                    fitness_fn = fitness_coords,
+                                    maximize=False)
+
+        # Solve problem using the genetic algorithm
+        best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state = 2)
+
+        print('The best state found is: ', best_state)
+        print('The fitness at the best state is: ', best_fitness)
+
+
+        # Begin dimension increase for 2D coordinates (px, py)
+        poses = []
+        for e in best_state:
+            arr_2D = np.array([self.coords_list[e][0], self.coords_list[e][1], 0, 1])
+            dim_incr = np.matmul(self.M_inv, arr_2D)
+
+            # pose appended to poses (Pose Array)
+            p = Pose()
+            p.position.x = dim_incr[0]
+            p.position.y = dim_incr[1]
+            p.position.z = dim_incr[2]
+            p.orientation.x = 0.0
+            p.orientation.y = 0.0
+            p.orientation.z = 0.0
+            p.orientation.w = 1.0
+            poses.append(p)
+
+        print(poses)
+if __name__=="__main__":
+    # Initialize the node
+    rospy.init_node('optimizer')
+    Optimizer()
+    rospy.spin()
